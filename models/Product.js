7f40cf3d103d3fa72170c7274f8c1e3f70cbff47
Erin DeVine <erinj291@gmail.com>
2024-08-05 00:24:49 -0400

    added in sequelize connections and made the models

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNUll: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+
   },
+  
   {
     sequelize,
     timestamps: false,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,9 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name']
+  })
 });
 
 router.get('/:id', (req, res) => {
