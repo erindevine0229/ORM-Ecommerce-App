48cd0a06e3a8ac7032722d821651766ff4dba8a5
Erin DeVine <erinj291@gmail.com>
2024-08-05 19:28:57 -0400

    finished Product routes and successfully added db and started server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,58 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err, "An error occurred when trying to find all products.");
+    res.status(500).json(err)
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      ]
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: "Unable to locate product with specified id."});
+      return;
+    } else {
+      res.json(productData);
+    }
+  })
+  .catch(err => {
+    console.log(err, "Unable to find product with that id.");
+    res.status(500).json(err);
+});
 });
 
 // create new product
@@ -25,6 +71,27 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+  Product.create(req.body)
+  .then((newProduct) => {
+    if (req.body.tagIds.length) {
+      const newProductTagArray = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      return ProductTag.bulkCreate(newProductTagArray);
+    }
+    res.status(200).json(newProduct);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch(err => {
+      console.log(err, "An erorr occurred when trying to create new product.");
+      res.status(400).json(err);
+  });
+ 
+
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -94,6 +161,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: "Unable to find a product with this id." });
+      return;
+    } else {
+      res.json(productData);
+    }
+  })
+  .catch(err => {
+    console.log(err, "Not able to delete product at this time.");
+    res.status(500).json(err);
+
+  });
 });
 
 module.exports = router;
