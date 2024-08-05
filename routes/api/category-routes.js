98a6a53f7604b6ff0b32b03a48aac46032eb9e90
Erin DeVine <erinj291@gmail.com>
2024-08-05 01:14:48 -0400

    updated routes for tag and category. Need to add product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,25 +7,104 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    attributes: ['id', 'category_name']
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
   })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err, "An error occurrec when trying to display all categories.")
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'Unable to locate category with specified id.' })
+      return;
+    } else {
+      res.json(categoryData)
+    };
+  })
+  .catch(err => {
+    console.log(err, "An error occurred when trying to find selected category.");
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err, "An error occurrec when trying to add new category.");
+    res.status(500).json(err);
+  });
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "Unable to locate category with specified id." });
+      return;
+    } else {
+      res.json(categoryData);
+      console.log('Successfully updated category!')
+    }
+  })
+  .catch(err => {
+    console.log(err, "Unable to update category.");
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'Unable to locate category with specified id.'});
+      return;
+    } else {
+      res.json(categoryData);
+    }
+  })
+  .catch(err => {
+    console.log(err, "Unable to delete selected category.");
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,108 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err, "An error occurred when finding all tags.");
+    res.status(500).json(err);
+  });
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "Unable to locate a tag with specified id." });
+      return;
+    } else {
+      res.json(tagData);
+    }
+  })
+  .catch(err => {
+    console.log(err, "Unable to find specified tag.");
+    res.status(500).json(err);
+  });
 });
+
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => {
+    console.log('New tag successfully created.');
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err, 'Unable to create new tag.');
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "Unable to find tag with specified id."});
+      return;
+    } else {
+      res.json(tagData)
+    }
+  })
+  .catch(err => {
+    console.log(err, "Unable to update tag.");
+    res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "Unable to locate tag with specified id."});
+      return;
+    } else {
+      res.json(tagData)
+    }
+  })
+  .catch(err => {
+    console.log(err, 'Unable to delete specified tag.');
+    res.status(500).json(err);
+});
 });
 
 module.exports = router;
